23a85992a07d677ee277ad354f5bf712157fb327
pxdnbluesoul <daniel.tharp@gmail.com>
2016-10-29 10:41:43 -0600

    Added Dashboard link to navbar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    &nbsp;
+                    <li><a href="/home">Dashboard</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
