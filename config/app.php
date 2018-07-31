febe2da78387ba1634b63a0525900a30057a3942
pxdnbluesoul <github@nags.me>
2018-07-31 14:37:22 -0600

    Upgrade laravel 5.3->5.4

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Laravel\Tinker\TinkerServiceProvider::class,
 
         /*
          * Application Service Providers...
